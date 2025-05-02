e5ce5b2fc6900682c80cd6aae89333546c9bb59c
gayLu <103807653+gayLu@users.noreply.github.com>
2025-05-02 18:00:32 +0000

    Deploying to gh-pages from @ gayLu/nav@7128cf6535f495b4a40c2087c4c9eea47edd492d 🚀

---
+++ chunk-NLT7AHJZ.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-DA35CPLU.js";import{Fb as e,vb as r}from"./chunk-KZB7AP3N.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
